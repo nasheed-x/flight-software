b46a1e5d1c81e4b7c2720e8ba7314949027b98aa
tm2904 <tm2904@nyu.edu>
2021-04-08 19:28:12 +0900

    minor edits to output

--- src/lps25.hpp
+++ src/lps25.hpp
@@ -5,7 +5,9 @@ void lps25_retrieve_data(){
   sensors_event_t temp;
   sensors_event_t pressure;
   lps.getEvent(&pressure, &temp);// get pressure
-  Serial.print("Temperature: ");Serial.print(temp.temperature);Serial.println(" degrees C");
-  Serial.print("Pressure: ");Serial.print(pressure.pressure);Serial.println(" hPa");
-  Serial.println("");
+  Serial.println("LPS25 DATA RETRIEVED");
+  Serial.print(" Temperature: ");Serial.print(temp.temperature);Serial.println(" degrees C");
+  Serial.print(" Pressure: ");Serial.print(pressure.pressure);Serial.println(" hPa");
+  Serial.println();
+  // Serial.println("");
 }
--- src/ublox.hpp
+++ src/ublox.hpp
@@ -2,9 +2,9 @@
 
 void printPVTdata(UBX_NAV_PVT_data_t ubxDataStruct)
 {
-    Serial.println();
+    Serial.println("UBLOX DATA RETRIEVED");
 
-    Serial.print(F("Time: ")); // Print the time
+    Serial.print(F(" Time: ")); // Print the time
     uint8_t hms = ubxDataStruct.hour; // Print the hours
     if (hms < 10) Serial.print(F("0")); // Print a leading zero if required
     Serial.print(hms);
@@ -20,19 +20,20 @@ void printPVTdata(UBX_NAV_PVT_data_t ubxDataStruct)
     unsigned long millisecs = ubxDataStruct.iTOW % 1000; // Print the milliseconds
     if (millisecs < 100) Serial.print(F("0")); // Print the trailing zeros correctly
     if (millisecs < 10) Serial.print(F("0"));
-    Serial.print(millisecs);
+    Serial.println(millisecs);
 
     long latitude = ubxDataStruct.lat; // Print the latitude
     Serial.print(F(" Lat: "));
-    Serial.print(latitude);
+    Serial.println(latitude);
 
     long longitude = ubxDataStruct.lon; // Print the longitude
     Serial.print(F(" Long: "));
     Serial.print(longitude);
-    Serial.print(F(" (degrees * 10^-7)"));
+    Serial.println(F(" (degrees * 10^-7)"));
 
     long altitude = ubxDataStruct.hMSL; // Print the height above mean sea level
     Serial.print(F(" Height above MSL: "));
     Serial.print(altitude);
     Serial.println(F(" (mm)"));
+    Serial.println();
 }
