f4147a2e8bc2144694c82c60d78e9d13a583bf0a
niniack <nishantaswani@nyu.edu>
2021-04-28 20:49:33 +0400

    Added LPS pins to config

--- src/config.hpp
+++ src/config.hpp
@@ -2,6 +2,15 @@
 
 // If needed change to 434.0 or other frequency, must match RX's freq!
 #define RF69_FREQ 915.0
+#define PWM_DRIVER_ADDRESS 0x70
+
+// For SPI mode, we need a CS pin
+#define LPS_CS PC6
+// For software-SPI mode we need SCK/MOSI/MISO pins
+#define LPS_SCK PA5
+#define LPS_MISO PA6
+#define LPS_MOSI PA7
+
 
 #if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
   #define RFM69_INT     3  // 
@@ -16,3 +25,5 @@
   #define RFM69_RST     PA10  // "A"
   #define LED           PA5
 #endif
+
+
