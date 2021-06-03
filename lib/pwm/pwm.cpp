6bd69d68927afe451514513f984d37689b4c98da
niniack <nishantaswani@nyu.edu>
2021-06-03 10:46:35 +0400

    Add comments for register modes in PWM driver

--- lib/pwm/pwm.cpp
+++ lib/pwm/pwm.cpp
@@ -4,8 +4,17 @@ PWMControl::PWMControl() : Task(TASK_IMMEDIATE, TASK_FOREVER, &scheduler, false)
 {
     this->driver = new PCA9635(0x70);
     // Inversion necessary!
+    // 0b00010101 == 21 (only inverts and sets frequency to 190Hz)
+    // 0b00110101 == 53 (inverts + and sets the DMBLNK bit)
+    // 0b00000101 == 5
+    // 0b00100101 == 37
+
+    // 0b00010000
+
     // See https://www.nxp.com/docs/en/data-sheet/PCA9635.pdf#page=17
-    this->driver->writeMode(PCA9635_MODE2, 21);
+    this->driver->writeMode(PCA9635_MODE1, 0b10000001);
+    this->driver->writeMode(PCA9635_MODE2, 53);
+
     // Initialize
     this->driver->begin();
     // Set all channels off
